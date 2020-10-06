79956d899cade95268fbd08af73d0532fd8595eb
ankoye <ankoye@qq.com>
2020-10-06 23:09:46 +0800

    update: 优化商品页面UI

--- young-tao-app/pages/goods/goods.vue
+++ young-tao-app/pages/goods/goods.vue
@@ -1,5 +1,6 @@
 <template>
-	<view>
+	<view class="container">
+		<!-- 导航栏 -->
 		<u-navbar class="goods-navbar" :immersive="true" :background="{backgroundColor: 'rgba(255,255,255,' + navbar.opacity + ')'}">
 			<view class="navbar-content" v-if="navbar.scrollTop > 10" :style="{opacity: navbar.opacity}">
 			  <view class="content-item" :class="{'active': tabIndex === 0}">
@@ -22,22 +23,153 @@
 				<!-- #endif -->
 			</view>
 			<view slot="right" class="navbar-right" :class="{'navbar-show': navbar.scrollTop > navbar.threshold}">
-				<view class="right-item" :style="'backgroundColor: rgba(0,0,0, '+(0.3-navbar.opacity)+')'">
-					<text class="iconfont icon-huaban"></text>
-				</view>
-				<view class="right-item" :style="'backgroundColor: rgba(0,0,0, '+(0.3-navbar.opacity)+')'">
-					<text class="iconfont icon-huaban"></text>
-				</view>
 				<!-- #ifndef MP -->
 				<view class="right-item" :style="'backgroundColor: rgba(0,0,0, '+(0.3-navbar.opacity)+')'">
 					<text class="iconfont icon-huaban"></text>
 				</view>
 				<!-- #endif -->
+				<view class="right-item" :style="'backgroundColor: rgba(0,0,0, '+(0.3-navbar.opacity)+')'">
+					<text class="iconfont icon-huaban"></text>
+				</view>
+				<view class="right-item" :style="'backgroundColor: rgba(0,0,0, '+(0.3-navbar.opacity)+')'">
+					<text class="iconfont icon-huaban"></text>
+				</view>
 			</view>
 		</u-navbar>
 		
+		<!-- 商品信息 -->
+		<view class="goods-info-wrap">
+			<u-swiper :list="list" border-radius="0" height="750" mode="number" indicator-pos="bottomRight"></u-swiper>
+			<view class="goods-info-box">
+				<!-- 商品价格 -->
+				<view class="price-wrap">
+					<view class="price-box">
+						<view class="price">￥200.00</view>
+						<view class="old-price">￥200.00</view>
+					</view>
+					<view class="price-right">
+						<view class="iconfont icon-huaban"></view>
+						<view>降价通知</view>
+					</view>
+					<view class="price-right">
+						<view class="iconfont icon-huaban"></view>
+						<view>收藏</view>
+					</view>
+				</view>
+				<!-- 商品标题 -->
+				<view class="title-box">
+					<u-tag class="title-tag" text="新品" type="success" size="mini" mode="dark" />
+					<text class="title">
+						vivo iQOO Z1x 水漾白 6GB+128GB 骁龙765G 120Hz竞速屏 5000mAh超大电池 双模5G全网通手机 iqooz1x
+					</text>
+				</view>
+			</view>
+		</view>
 		
-		<view v-for="i in 20" :key="i" style="padding: 30rpx;">a</view>
+		<!-- 优惠、服务 -->
+		<view class="serve-wrap">
+			<view class="discount-box">
+				<view>优惠</view>
+				<view class="discount-content">
+					<view>樱桃立减70元优惠券</view>
+					<view>满99-19  满49-10</view>
+					<view>goumai 1件既可换购热销商品</view>
+				</view>
+				<view>...</view>
+			</view>
+			<view class="serve-box">
+				<view>服务</view>
+				<view class="serve-content">
+					<text>假一赔三</text>
+					<text>只换不修</text>
+				</view>
+				<view>...</view>
+			</view>
+		</view>
+		
+		<!-- 配送 -->
+		<view class="delivery-wrap">
+			<view class="selected-box">
+				<view>已选</view>
+				<view class="content">
+					xxxx
+				</view>
+				<view>...</view>
+			</view>
+			<view class="ship-box">
+				<view>发货</view>
+				<view class="content">
+					xxxx
+				</view>
+				<view>...</view>
+			</view>
+			<view class="destination-box">
+				<view>送至</view>
+				<view class="content">
+					xxxx
+				</view>
+				<view>...</view>
+			</view>
+		</view>
+		
+		<!-- 评价 -->
+		<view class="comment-wrap">
+			<!-- 评价 -->
+			<view class="comment-box">
+				<u-section title="评价" sub-title="好评度90%"></u-section>
+				<view>标签1、标签2</view>
+				<view>
+					评价内容
+				</view>
+			</view>
+			<!-- 问大家 -->
+			<view class="answer-box">
+				<u-section title="问大家" sub-title="查看全部"></u-section>
+				<view>list</view>
+				<view>list</view>
+			</view>
+		</view>
+		
+		<!-- 店铺 -->
+		<view class="shop-wrap">
+			<view class="shop-box">
+				<view class="image">
+					image
+				</view>
+				<view class="content">
+					<view>店名</view>
+					<view>
+						<text>樱桃</text>
+						综合体验 ...
+					</view>
+				</view>
+				<view class="evaluate">
+					<view>宝贝描述5.0高</view>
+					<view>卖家服务5.0高</view>
+					<view>物流服务5.0高</view>
+				</view>
+			</view>
+			<view class="recommend-box">
+				<u-section title="店铺推荐" sub-title="查看全部"></u-section>
+				<view>xxx</view>
+			</view>
+		</view>
+		
+		<!-- 详情 -->
+		<view class="detail-wrap">
+			<u-divider>宝贝详情</u-divider>
+			<view>...</view>
+		</view>
+		
+		<!-- 推荐 -->
+		<view class="recommend-wrap">
+			<u-divider>看了又看</u-divider>
+			<view>...</view>
+		</view>
+		
+		<view class="goods-bar">
+			<yt-goodsbar></yt-goodsbar>
+		</view>
 	</view>
 </template>
 
@@ -52,7 +184,12 @@ export default {
 				scrollTop: 0,
 				opacity: 0,
 				threshold: 100
-			}
+			},
+			list: [
+				{ image: 'https://cdn.uviewui.com/uview/swiper/1.jpg' },
+				{ image: 'https://cdn.uviewui.com/uview/swiper/2.jpg' },
+				{ image: 'https://cdn.uviewui.com/uview/swiper/3.jpg' }
+			],
     };
   },
 	onPageScroll(e) {
@@ -68,6 +205,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+	margin-bottom: calc(var(--window-bottom) + 125rpx);
+}
 .goods-navbar {
 	.navbar-content {
 		width: 100%;
@@ -113,5 +253,123 @@ export default {
 			}
 		}
 	}
+}
+.goods-info-wrap {
+	margin-bottom: $module-margin;
+	.goods-info-box {
+		background-color: #FFF;
+		padding: 20rpx;
+	}
+	.price-wrap {
+		margin-bottom: $module-margin;
+		display: flex;
+	}
+	.price-box {
+		flex: 1;
+		display: flex;
+		align-items: baseline;
+		.price {
+			font-size: 60rpx;
+			color: $primary-color;
+			margin-right: 10rpx;
+		}
+		.old-price {
+			font-size: 26rpx;
+			text-decoration: line-through;
+		}
+	}
+	.price-right {
+		text-align: center;
+		margin-right: 10rpx;
+	}
+	.title-box {
+		.title-tag {
+			margin-right: 10rpx;
+		}
+		.title {
+			font-size: 32rpx;
+			color: #262626;
+			font-weight: bolder;
+		}
+	}
+}
+.serve-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+	.discount-box {
+		display: flex;
+		.discount-content {
+			flex: 1;
+		}
+	}
+	.serve-box {
+		display: flex;
+		.serve-content {
+			flex: 1;
+		}
+	}
+}
+.delivery-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+	.selected-box {
+		display: flex;
+		.content {
+			flex: 1;
+		}
+	}
+	.ship-box {
+		display: flex;
+		.content {
+			flex: 1;
+		}
+	}
+	.destination-box {
+		display: flex;
+		.content {
+			flex: 1;
+		}
+	}
+}
+.comment-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+	.comment-box {
+	}
+	.answer-box {
+	}
+}
+.shop-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+	.shop-box {
+		display: flex;
+		.content {
+			flex: 1;
+		}
+	}
+}
+.shop-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+}
+.detail-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+}
+.recommend-wrap {
+	margin-bottom: $module-margin;
+	background-color: #fff;
+	padding: 20rpx;
+}
+.goods-bar {
+	position: fixed;
+	bottom: var(--window-bottom);
 }
 </style>
